367b27b2411f0983aee850cc46d878024106b489
sharif <sharif@ajab.sharifulislam.com>
2024-01-08 20:50:20 +0300

    compound component pattern

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
-        iconIncrease="+"
-        iconDecrease="-"
-        label="My NOT so flexible counter"
-        hideLabel={false}
-        hideIncrease={false}
-        hideDecrease={false}
-      />
+      <Counter>
+        <Counter.Label>Count</Counter.Label>
+        <Counter.Decrease icon='-' />
+        <Counter.Count />
+        <Counter.Increase icon='+' />
+      </Counter>
     </div>
   );
 }
